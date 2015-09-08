6a0921f45942ab2cbe2d5bea732f2ae2623df31b
Student <student@RU-019.local>
2015-09-08 09:55:41 -0600

    vars added

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 var word = prompt("Please enter in a single word")
 	console.log(word)
+
+	var response = "The word you entered was: " + word
+	var length = word.length
+	var third = word.charAt(2)
+	var lowerCase = word.toLowerCase
+	var upperCase = word.toUpperCase
+	var sentence = "I know that " + word + "is a word."
+	var substring = word.substring(1, 4)
+	
+
+alert ( "The word you entered was: " + word, word.length);
+
+		
